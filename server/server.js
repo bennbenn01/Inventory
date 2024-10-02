7e5352dcef0d2eab1e680f04d4dd4a6e7a492da6
bennbenn01 <bennyberaldel@gmail.com>
2024-10-02 23:33:23 +0800

    Updated

--- server/server.js
+++ server/server.js
@@ -4,11 +4,14 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import path from 'path'
 import users from './routes/api/users.js'
+import { fileURLToPath } from 'url'
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
@@ -21,14 +24,21 @@ mongoose.connect(process.env.MONGO_API,{
 .then(()=> console.log('MongoDB Connected'))
 .catch(error => console.error(error));
 
-app.use('/users', users);
 
-app.get('/', (req, res)=>{
-    res.send('The Server is online.');  
+app.use((req, res, next)=>{
+    const allowedOrigin = process.env.PORT;
+    const origin = req.headers.origin;
+
+    if(origin === allowedOrigin){
+        next(app.use('/users', users));
+    }else{
+        res.sendFile(path.join(__dirname, 'Views', '403.html'));
+    }
+
 });
 
-app.get('/404(.html)?', (req, res)=> {
-    res.sendFile(path.join(__dirname, 'Views', '404.html'));
+app.all('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, 'Views', '403.html'));
 });
 
 app.listen(PORT, ()=>{
