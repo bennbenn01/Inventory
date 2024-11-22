a80bb456696cdb7dc0fdd432ffbc0f69cc16c4d8
bennbenn01 <bennyberaldel@gmail.com>
2024-11-22 15:53:44 +0800

    First Commit

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 mongoose.connect(process.env.MONGO_API,{
