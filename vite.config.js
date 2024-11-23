99d54944d1ab736d8eebf612bf7bbfe4cf1321b1
bennbenn01 <bennyberaldel@gmail.com>
2024-11-23 17:55:22 +0800

    Updated

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Inventory/',
   define: {
     'process.env': process.env,
-  },
-  build: {
-    outDir: 'dist',  
-    sourcemap: true,
   },
 })
